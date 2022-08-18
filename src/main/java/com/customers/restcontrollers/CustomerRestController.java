fb3572072fcb470913d799fcf8b7b30345fbf15f
Alfonso José Barroso Barajas <alfonsobarrosobarajas@gmail.com>
2022-08-18 10:07:06 -0600

    First Commit

---
+++ src/main/java/com/customers/restcontrollers/CustomerRestController.java
@@ -0,0 +1,15 @@
+package com.customers.restcontrollers;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/customer")
+public class CustomerRestController {
+
+    @GetMapping()
+    public String findAll(){
+        return "Customers";
+    }
+}
